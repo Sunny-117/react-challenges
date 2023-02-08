8515b5f2effc1dacc43fbd0d0545bdc66ba4a192
付志强 <zhiqiangfu6@gmail.com>
2023-02-08 19:46:02 +0800

    fix: react board

--- react-app/src/Board.tsx
+++ react-app/src/Board.tsx
@@ -1,28 +1,60 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Square from "./Square";
+import { calculateWinner } from "./utils";
 
-function renderSquare(i) {
-  return <Square value={i} />;
-}
+const Board: React.FC = () => {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  const [xIsNext, setXIsNext] = useState(true);
+  const [status, setStatus] = useState<null | string>(null);
+  function renderSquare(i: number) {
+    const handleClick = (i: number) => {
+      const squaresArr = squares.slice();
+      console.log(squaresArr);
+      squaresArr[i] = xIsNext ? "X" : "O";
+      setXIsNext(!xIsNext);
+      setSquares(squaresArr);
+    };
+    return (
+      <Square
+        value={squares[i]}
+        onClick={() => {
+          handleClick(i);
+        }}
+      />
+    );
+  }
 
-export default function Board() {
+  useEffect(() => {
+    const winner = calculateWinner(squares);
+    if (winner) {
+      setStatus("Winner" + winner);
+    } else {
+      setStatus("next player" + (xIsNext ? "X" : "O"));
+    }
+  }, [squares]);
+
   return (
     <div>
-      <div className="board-row">
-        {renderSquare(0)}
-        {renderSquare(1)}
-        {renderSquare(2)}
-      </div>
-      <div className="board-row">
-        {renderSquare(3)}
-        {renderSquare(4)}
-        {renderSquare(5)}
-      </div>
-      <div className="board-row">
-        {renderSquare(6)}
-        {renderSquare(7)}
-        {renderSquare(8)}
+      <p> {status}</p>
+      <div>
+        <div className="board-row">
+          {renderSquare(0)}
+          {renderSquare(1)}
+          {renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {renderSquare(3)}
+          {renderSquare(4)}
+          {renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {renderSquare(6)}
+          {renderSquare(7)}
+          {renderSquare(8)}
+        </div>
       </div>
     </div>
   );
-}
+};
+
+export default Board;
--- react-app/src/Square.tsx
+++ react-app/src/Square.tsx
@@ -1,7 +1,22 @@
+import { useState } from "react";
+
 interface ISquare {
   value: number;
+  onClick: (e?: any) => void;
 }
-const Square: React.FC<ISquare> = ({ value }) => {
-  return <button className="square">{value}</button>;
+const Square: React.FC<ISquare> = ({ value, onClick }) => {
+  const [squareValue, setSquareValue] = useState<null | string>(null);
+  return (
+    <button
+      className="square"
+      onClick={() => {
+        setSquareValue("X");
+        onClick();
+      }}
+    >
+      {value}
+    </button>
+  );
 };
+
 export default Square;
