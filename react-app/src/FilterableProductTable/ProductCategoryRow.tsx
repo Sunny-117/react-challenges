8f23c08a720e0b3c682a90779953bd52047eebf6
Sunny-117 <zhiqiangfu6@gmail.com>
2024-08-23 17:33:56 +0800

    feat: remove app

--- react-app/src/FilterableProductTable/ProductCategoryRow.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-// 为每一个产品类别展示标题
-export default function ProductCategoryRow({ title }: any) {
-  return <div>{title}</div>;
-}
--- react-app/src/FilterableProductTable/ProductRow.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function ProductRow({ keyName, value }: any) {
-  return (
-    <li>
-      {keyName}:{value}
-    </li>
-  );
-}
